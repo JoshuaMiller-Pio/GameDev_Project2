4086dfa7a57a7ba39745caf32bec88bbc7aaef8e
WinnieThePio <81216801+WinnieThePio@users.noreply.github.com>
2022-10-07 18:23:12 +0200

    added gold and item

---
+++ GameDev_Project2/Gold.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDev_Project2
+{
+    internal class Gold:Item
+    {
+       private int currentgold;
+       private Random random;
+
+        public Gold(int x, int y, int CurrentGold ):base(x,y, TileType.Gold, 3)
+        {
+            random = new Random();
+            CurrentGold = random.Next(1, 6);
+            currentgold = CurrentGold;
+        }
+
+        #region accessors and mutators
+        private void setCurrentGold(int gold)
+        {
+          currentgold = gold;  
+        }
+
+        public int getCurrentGold() 
+        {
+            return currentgold;
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return String.Format("Current Gold:{1}",this.getCurrentGold());
+        }
+
+    }
+}
---
+++ GameDev_Project2/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDev_Project2
+{
+    public abstract class Item : Tile
+    {
+        public Item(int x, int y, TileType type, int SymbolIndex) : base(x, y, type, SymbolIndex)
+        {
+
+        }
+        public abstract override string ToString();
+    }
+}
