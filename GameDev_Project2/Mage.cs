b61bb215699d22ceacb857d9f94e9765c5070393
WinnieThePio <81216801+WinnieThePio@users.noreply.github.com>
2022-10-21 19:52:23 +0200

    blalfgaewuibhnopwerg;iobjnuaWERGbjuhi;wergvbnijou;awergvbinoju;aGWER

--- GameDev_Project2/Mage.cs
+++ GameDev_Project2/Mage.cs
@@ -23,78 +23,21 @@ namespace GameDev_Project2
 
         public override bool CheckRange(Character h, Tile target)
         {
-            //Sets the Inrange boolean to false as the default everytime checkrange begins
-            InRange = false;
-
-            //Creates a Tile array that stores all tiles that fall within the mages attack radius
-
-            Tile[] MagesTargets = new Tile[7];
-
-            //populates the MagesTargets tile array with the correct tiles by setting the co-ordinates of each of the apropriate tiles within the array with reference to the apropriate mages co-ordinates (Mage being the character h)
-           
-            for (int i = 0; i < MagesTargets.Length; i++)
+         
+            double CurrentRange = h.GetRange();
+            int CurrentDistance = CheckDistanceToTarget(h, target);
+            Console.WriteLine(CurrentRange);
+            if (CurrentDistance == CurrentRange || (CurrentDistance * -1) == CurrentRange)
             {
-                if(i == 0)
-                {
-                    MagesTargets[i].SetX(h.GetX() - 1);
-                    MagesTargets[i].SetY(h.GetY() - 1);
-                    
-                }
-
-                if (i == 1)
-                {
-                    MagesTargets[i].SetX(h.GetX());
-                    MagesTargets[i].SetY(h.GetY() - 1);
-                }
-
-                if (i == 2)
-                {
-                    MagesTargets[i].SetX(h.GetX() + 1);
-                    MagesTargets[i].SetY(h.GetY() - 1);
-                }
-
-                if (i == 3)
-                {
-                    MagesTargets[i].SetX(h.GetX() - 1);
-                    MagesTargets[i].SetY(h.GetY());
-                }
-
-                if (i == 4)
-                {
-                    MagesTargets[i].SetX(h.GetX() + 1);
-                    MagesTargets[i].SetY(h.GetY());
-                }
-
-                if (i == 5)
-                {
-                    MagesTargets[i].SetX(h.GetX() - 1);
-                    MagesTargets[i].SetY(h.GetY() + 1);
-                }
-
-                if (i == 6)
-                {
-                    MagesTargets[i].SetX(h.GetX());
-                    MagesTargets[i].SetY(h.GetY() + 1);
-                }
-
-                if (i == 7)
-                {
-                    MagesTargets[i].SetX(h.GetX() + 1);
-                    MagesTargets[i].SetY(h.GetY() + 1);
-                }
+                InRange = true;
             }
-            //Compares the tile type of each of the tiles within the MagesTargets array and if they are a suitable type (Hero or Enemy) sets the InRange boolean to true
-            for (int j = 0; j < MagesTargets.Length; j++)
+            else
             {
-                if(MagesTargets[j].GetCurrentTileType() == TileType.Enemy || MagesTargets[j].GetCurrentTileType() == TileType.Hero)
-                {
-                    InRange = true;
-                }
+                InRange = false;
             }
-            
-            //returns the InRange boolean
             return InRange;
-        }
+        
+    }
     
     }
 }
